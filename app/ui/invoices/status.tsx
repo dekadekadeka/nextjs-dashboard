3cde7b205f1e3bf949eb47d135377c34234de2c1
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2024-05-24 12:03:00 -0400

    initial

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -12,18 +12,18 @@ export default function InvoiceStatus({ status }: { status: string }) {
         },
       )}
     >
-      {status === 'pending' ? (
+      {status === 'pending' && (
         <>
           Pending
           <ClockIcon className="ml-1 w-4 text-gray-500" />
         </>
-      ) : null}
-      {status === 'paid' ? (
+      )}
+      {status === 'paid' && (
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
         </>
-      ) : null}
+      )}
     </span>
   );
 }
